397442a05d83314cb479af4f79cee3b220d468e0
cami oliva <lamica.olivares@gmail.com>
2024-10-24 12:09:59 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-     
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <NavLink to="/">Inicio</NavLink>
+      <NavLink to="/blog">Blog</NavLink>
+      <NavLink to="/tienda">Acerca de</NavLink>
     </div>
   );
 }
